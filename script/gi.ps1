a4e5417d2aa30a3af7a91cfa22e0d6fe68825d00
eric.kuo <fine1101105351@gmail.com>
2026-03-01 16:48:27 +0800

    docs: add script

--- script/gi.ps1
+++ script/gi.ps1
@@ -1,1 +1,3 @@
 python main.py multi --folder "F:\天狼之章" --desc "劇情" --tags "原神,gensinimpact,天狼之章,法爾伽" --playlist "原神"
+
+python main.py multi --folder "F:\2026-03-01-幻想真境劇詩" --desc "練度展示於結尾" --tags "原神,gensinimpact" --playlist "原神-高難"
