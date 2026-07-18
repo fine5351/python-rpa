37555577e10e4724eb759dd42d9b4ce1c25fc24a
eric.kuo <fine1101105351@gmail.com>
2026-07-18 10:51:39 +0800

    feat: add PowerShell script for GI automation process

--- selenium_impl/script/gi.ps1
+++ selenium_impl/script/gi.ps1
@@ -1,7 +1,4 @@
 python ./selenium_impl/main.py multi --folder "F:\原神-月之七-虛空劫灰往世書" --desc "劇情" --tags "原神,gensinimpact,月之七,虛空劫灰往世書" --playlist "原神"
-
-python ./selenium_impl/main.py multi --folder "F:\傳說任務-狡兔之章" --desc "劇情" --tags "原神,gensinimpact,洛恩,狡兔之章" --playlist "原神"
-
-python ./selenium_impl/main.py multi --folder "F:\2026-06-06-幻想真境劇詩" --desc "練度展示於結尾" --tags "原神,gensinimpact" --playlist "原神-高難"
-
-python ./selenium_impl/main.py multi --folder "F:\2026-06-18-深境螺旋" --desc "練度展示於結尾" --tags "原神,gensinimpact" --playlist "原神-高難"
+# python ./selenium_impl/main.py multi --folder "F:\傳說任務-狡兔之章" --desc "劇情" --tags "原神,gensinimpact,洛恩,狡兔之章" --playlist "原神"
+# python ./selenium_impl/main.py multi --folder "F:\2026-06-06-幻想真境劇詩" --desc "練度展示於結尾" --tags "原神,gensinimpact" --playlist "原神-高難"
+# python ./selenium_impl/main.py multi --folder "F:\2026-07-18-深境螺旋" --desc "練度展示於結尾" --tags "原神,gensinimpact" --playlist "原神-高難"
